b3bb2556a5a99366d97c244c1b22904fa05f8e4b
Rohith P <dm.rohithp@gmail.com>
2026-01-26 12:21:52 +0530

    Added State

---
+++ session/state.go
@@ -0,0 +1,59 @@
+package session
+
+import (
+	"backgroundcc/ledbatpp"
+	"backgroundcc/pacing"
+	"backgroundcc/transport"
+	"net"
+	"sync"
+	"time"
+)
+
+type SessionState struct {
+	Mutex      sync.Mutex
+	LocalAddr  *net.UDPAddr
+	RemoteAddr *net.UDPAddr
+	Transport  *transport.ReliableTransport
+	Pacer      *pacing.Pacer
+	Controller *ledbatpp.Controller
+	StartTime  time.Time
+	Closed     bool
+
+	StopChan chan struct{}
+}
+
+func NewSessionState(
+	LocalAddr *net.UDPAddr,
+	RemoteAddr *net.UDPAddr,
+	Transport *transport.ReliableTransport,
+	Pacer *pacing.Pacer,
+	Controller *ledbatpp.Controller,
+) *SessionState {
+	return &SessionState{
+		LocalAddr:  LocalAddr,
+		RemoteAddr: RemoteAddr,
+		Transport:  Transport,
+		Pacer:      Pacer,
+		Controller: Controller,
+		StartTime:  time.Now(),
+		Closed:     false,
+		StopChan:   make(chan struct{}),
+	}
+}
+
+func (S *SessionState) IsClosed() bool {
+	S.Mutex.Lock()
+	defer S.Mutex.Unlock()
+	return S.Closed
+}
+
+func (S *SessionState) Close() {
+	S.Mutex.Lock()
+	if S.Closed {
+		S.Mutex.Unlock()
+		return
+	}
+	S.Closed = true
+	close(S.StopChan)
+	S.Mutex.Unlock()
+}
