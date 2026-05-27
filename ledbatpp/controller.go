d346cb08269ed16086515b03e997839bfad95073
Rohith P <dm.rohithp@gmail.com>
2026-05-28 01:26:35 +0530

    Update controller.go

--- ledbatpp/controller.go
+++ ledbatpp/controller.go
@@ -3,11 +3,11 @@ package ledbatpp
 import "math"
 
 type Controller struct {
-	params Params
+	params Params  // controller parameters
 	clock  Clock
 }
 
-func NewController(params Params, clock Clock) *Controller {
+func NewController(params Params, clock Clock) *Controller {  // constructor
 	return &Controller{
 		params: params,
 		clock:  clock,
@@ -15,8 +15,8 @@ func NewController(params Params, clock Clock) *Controller {
 }
 
 func (c *Controller) Update(state *State) {
-	now := c.clock.Now()
-	dt := now.Sub(state.LastRateUpdate).Seconds()
+	now := c.clock.Now()  // current time 
+	dt := now.Sub(state.LastRateUpdate).Seconds()  // delta t = dt = now - lastupdate
 	if dt <= 0 {
 		return
 	}
@@ -25,19 +25,19 @@ func (c *Controller) Update(state *State) {
 	// This one tells us if we should increase or decrease the sending rate
 	// So its the most important part xD
 
-	err := state.TargetDelay - state.QueuingDelay
-	norm := err.Seconds() / state.TargetDelay.Seconds()
+	err := state.TargetDelay - state.QueuingDelay   // target delay - queuing delay
+	norm := err.Seconds() / state.TargetDelay.Seconds()  // normalize it for controller independent scaling
 	gain := math.Abs(norm)
 	if err > 0 {
-		state.Rate += c.params.AdditiveIncrease * gain * dt
+		state.Rate += c.params.AdditiveIncrease * gain * dt  // if positive error, i.e. queuing delay is less than target delay, increase rate of sending
 	} else {
 		state.Rate -= state.Rate * c.params.ProportionalDecrease * gain * dt
 	}
-	if state.Rate < c.params.MinRate {
-		state.Rate = c.params.MinRate
+	if state.Rate < c.params.MinRate {  // maximum rate clamp, i.e. if rate is less than minimum, then clamp it
+		state.Rate = c.params.MinRate  // rate is clamped
 	}
-	if state.Rate > c.params.MaxRate {
+	if state.Rate > c.params.MaxRate {  // same if it exceeds
 		state.Rate = c.params.MaxRate
 	}
-	state.LastRateUpdate = now
+	state.LastRateUpdate = now  // the last it was updated is now (needed for next dt computation)
 }
