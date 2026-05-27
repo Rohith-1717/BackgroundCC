6a0740eb52b4cd5d300e8f995f737a520e109469
Rohith P <dm.rohithp@gmail.com>
2026-05-28 00:01:12 +0530

    Update clock.go

--- ledbatpp/clock.go
+++ ledbatpp/clock.go
@@ -3,13 +3,13 @@ import "time"
 
 // This is our clock abstraction
 
-type Clock interface{  // the operations clock must support
+type Clock interface{  // the operations clock must support (IT IS AN INTERFACE BTW)
 	Now() time.Time	// current time
 	Since(t time.Time) time.Duration // how much time has passed since t
 	After(d time.Duration) <-chan time.Time	// for waking up after some delay
 }
 
-type MonotonicClock struct{}
+type MonotonicClock struct{} // this is our real Clock
 
 
 func NewMonotonicClock() *MonotonicClock{
