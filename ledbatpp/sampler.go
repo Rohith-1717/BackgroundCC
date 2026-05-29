428b405c8471bf7ab378e1edd387d82f1868e01b
Rohith P <dm.rohithp@gmail.com>
2026-05-29 17:14:13 +0530

    Update sampler.go

--- ledbatpp/sampler.go
+++ ledbatpp/sampler.go
@@ -1,14 +1,16 @@
 package ledbatpp
 import "time"
 
+// This computes the rtt
+
 type Sample struct{
-	SendTime time.Time
-	AckTime time.Time
-	RTT time.Duration
+	SendTime time.Time  // time we sent
+	AckTime time.Time  // time we got ACK
+	RTT time.Duration  // ack time - send time
 }
 
 type Sampler struct{
-	clock Clock
+	clock Clock  // Sampler's clock
 }
 
 func NewSampler(clock Clock) *Sampler{
@@ -16,10 +18,10 @@ func NewSampler(clock Clock) *Sampler{
 }
 
 func (s *Sampler) Observe(sendTime time.Time)(Sample, bool){
-	now := s.clock.Now()
+	now := s.clock.Now()  // ack arrival time 
 
 	if sendTime.After(now){
-		return Sample{}, false
+		return Sample{}, false  // if sent time is after now, then its not possible since sent time is after ack recieved time
 	}
 
 	rtt := now.Sub(sendTime)
@@ -27,7 +29,7 @@ func (s *Sampler) Observe(sendTime time.Time)(Sample, bool){
 		return Sample{}, false
 	}
 
-	return Sample{SendTime: sendTime,	AckTime: now, RTT: rtt,}, true
+	return Sample{SendTime: sendTime,	AckTime: now, RTT: rtt,}, true  // return the sent time, ack time and rtt
 }
 
 // This is to make sure that the RTTs that we observed make sense
