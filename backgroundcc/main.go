7c0f4d9044df1bdb8e71f119a7c3b21b46d7f50d
Rohith P <dm.rohithp@gmail.com>
2026-06-22 20:47:19 +0530

    main function and add sender/receiver logic

--- backgroundcc/main.go
+++ backgroundcc/main.go
@@ -12,17 +12,11 @@ import (
 	"time"
 )
 
-func main() {
-	if len(os.Args) >= 2 && os.Args[1] == "validate" {
-		runValidate()
-		return
-	}
-	if len(os.Args) < 4 {
-		log.Fatalf("usage: %s <local_addr> <remote_addr> <file>", os.Args[0])
-	}
-	localAddrStr := os.Args[1]
-	remoteAddrStr := os.Args[2]
-	filePath := os.Args[3]
+func runSender(
+	localAddrStr string,
+	remoteAddrStr string,
+	filePath string,
+) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -32,35 +26,57 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	remoteAddr, err := net.ResolveUDPAddr("udp", remoteAddrStr)
+	remoteAddr, err := net.ResolveUDPAddr(
+		"udp",
+		remoteAddrStr,
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	clock := ledbatpp.NewMonotonicClock()
 	params := ledbatpp.DefaultParams()
 	state := ledbatpp.NewState(
-		params.TargetDelay, params.MinRate,
+		params.TargetDelay,
+		params.MinRate,
 		clock.Now(),
 	)
-	controller := ledbatpp.NewController(params, clock)
-	startup := ledbatpp.NewStartup(clock, clock.Now())
-	slowdown := ledbatpp.NewSlowdown(params, clock)
+
+	controller := ledbatpp.NewController(
+		params,
+		clock,
+	)
+	startup := ledbatpp.NewStartup(
+		clock,
+		clock.Now(),
+	)
+	slowdown := ledbatpp.NewSlowdown(
+		params,
+		clock,
+	)
 	sampler := ledbatpp.NewSampler(clock)
 	estimator := ledbatpp.NewDelayEstimator(
 		params.BaseDelayWindow,
 		params.CurrentDelayWindow,
 	)
-	tracker := ledbatpp.NewDelayTracker(estimator, state, clock)
-	loss := ledbatpp.NewLoss(params, clock)
+	tracker := ledbatpp.NewDelayTracker(
+		estimator,
+		state,
+		clock,
+	)
+	loss := ledbatpp.NewLoss(
+		params,
+		clock,
+	)
 	rt := transport.NewReliableTransport(
 		udp,
 		sampler,
 		tracker,
 		loss,
 		state,
+		nil,
 	)
 	pacer := pacing.NewPacer(clock)
-
 	sess := session.NewSessionState(
 		nil,
 		remoteAddr,
@@ -72,11 +88,118 @@ func main() {
 		params,
 		state,
 	)
-	sender := session.NewSender(sess, reader, 1200)
-	log.Println("BackgroundCC started")
+	sender := session.NewSender(
+		sess,
+		reader,
+		1200,
+	)
+	log.Println("BackgroundCC sender started")
 	if err := sender.Run(); err != nil {
 		log.Fatal(err)
 	}
 	time.Sleep(100 * time.Millisecond)
-	log.Println("BackgroundCC finished")
+	log.Println("BackgroundCC sender finished")
+}
+
+func runReceiver(
+	localAddrStr string,
+	outputPath string,
+) {
+	receiver, err := transport.NewReceiver(
+		outputPath,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	udp, err := transport.NewUDPTransport(
+		localAddrStr,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clock := ledbatpp.NewMonotonicClock()
+	params := ledbatpp.DefaultParams()
+	state := ledbatpp.NewState(
+		params.TargetDelay,
+		params.MinRate,
+		clock.Now(),
+	)
+
+	sampler := ledbatpp.NewSampler(clock)
+	estimator := ledbatpp.NewDelayEstimator(
+		params.BaseDelayWindow,
+		params.CurrentDelayWindow,
+	)
+
+	tracker := ledbatpp.NewDelayTracker(
+		estimator,
+		state,
+		clock,
+	)
+
+	loss := ledbatpp.NewLoss(
+		params,
+		clock,
+	)
+
+	rt := transport.NewReliableTransport(
+		udp,
+		sampler,
+		tracker,
+		loss,
+		state,
+		receiver,
+	)
+	go rt.ReceiveLoop()
+	log.Println("BackgroundCC receiver started")
+	for !receiver.Done() {
+		time.Sleep(
+			100 * time.Millisecond,
+		)
+	}
+	rt.Close()
+	log.Println("BackgroundCC receiver finished")
+}
+
+func main() {
+	if len(os.Args) >= 2 &&
+		os.Args[1] == "validate" {
+		runValidate()
+		return
+	}
+	if len(os.Args) < 2 {
+		log.Fatal(
+			"usage:\n" +
+				"backgroundcc send <local_addr> <remote_addr> <file>\n" +
+				"backgroundcc receive <local_addr> <output_file>",
+		)
+	}
+
+	switch os.Args[1] {
+	case "send":
+		if len(os.Args) != 5 {
+			log.Fatal(
+				"usage: backgroundcc send <local_addr> <remote_addr> <file>",
+			)
+		}
+		runSender(
+			os.Args[2],
+			os.Args[3],
+			os.Args[4],
+		)
+	case "receive":
+		if len(os.Args) != 4 {
+			log.Fatal(
+				"usage: backgroundcc receive <local_addr> <output_file>",
+			)
+		}
+		runReceiver(
+			os.Args[2],
+			os.Args[3],
+		) 
+	default:
+		log.Fatal(
+			"unknown mode: use send or receive",
+		)
+	}
 }
